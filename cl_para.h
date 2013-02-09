941dd132c7e1088b2068740423d11137eff9e44c
Jakob Fischer <jakob@automorph.info>
2013-02-09 19:55:52 +0100

    Documentation

--- cl_para.h
+++ cl_para.h
@@ -1,3 +1,9 @@
+/* author: jakob fischer (jakob@automorph.info)
+ * date: 9th February 2013
+ * description: 
+ * Includes class for handling C++ console parameters.
+ */
+
 #ifndef CL_PARA_H
 #define CL_PARA_H
 
@@ -7,56 +13,82 @@
 #include <iostream>
 
 
+/* 
+ * Class for handling C++ console parameters.
+ */
+
 class cl_para {
+	// map that asocciates console parameters with its values
+	// parameters given without value get the value "empty"
     std::map<std::string, std::string> par;
   
 public:
+
+    /* Constructor gets the parameters exactly as given to the C(++)
+     * main function.
+     */
+
     cl_para(int argc, const char* argv[]) {
         for(size_t i=1; i<argc; ++i) { 
-	    std::string str(argv[i]);
-	    size_t fnd=str.find('=');
+	        std::string str(argv[i]);
+	        size_t fnd=str.find('=');
 	    
-	    if(fnd == str.npos) 
-	        par[str] = std::string("empty");  
-	    else 
-	        par[str.substr(0, fnd)] = str.substr(fnd+1); 
-	}
+	        if(fnd == str.npos) 
+	            par[str] = std::string("empty");  
+	        else 
+	            par[str.substr(0, fnd)] = str.substr(fnd+1); 
+	    }
     }
     
+    
+    /*
+     * Is parameter str in the parameter list?
+     */
     
     bool have_param(const std::string& str) {
         if(par[str].length() > 0) 
-	    return true;
-	else
-	    return false;
+	        return true;
+	    else
+	        return false;
     }
     
+    
+    /*
+     * 
+     */
     
     double get_param_d(const std::string& str) {
         double d;
       
-	std::istringstream ss(par[str]);
-	ss >> d;
+	    std::istringstream ss(par[str]);
+	    ss >> d;
       
         return d;  
     }
     
     
+    /*
+     * 
+     */
+    
     std::string get_param(const std::string& str) {
         return par[str];  
     }
     
+
+    /*
+     * 
+     */
     
     int get_param_i(const std::string& str) {
         int i;
       
-	std::istringstream ss(par[str]);
-	ss >> i;
+	    std::istringstream ss(par[str]);
+	    ss >> i;
       
         return i;  
     }
-  
-  
+
 };
 
 
