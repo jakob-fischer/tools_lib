02ea382893af9e875c8d33da4aa599a3dbe266d3
Jakob Fischer <jakob@automorph.info>
2017-05-04 17:21:26 +0200

    new homepage

--- cl_para.h
+++ cl_para.h
@@ -1,4 +1,4 @@
-/* author: jakob fischer (jakob@automorph.info)
+/* author: jakob fischer (mail@jakobfischer.eu)
  * date: 9th February 2013
  * homepage: https://github.com/jakob-fischer/tools_lib
  * description: 
